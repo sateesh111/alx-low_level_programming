2e3aead64ca37d32656630e8816c913e9abc5bec
sateesh111 <hussensateesh@gmail.com>
2021-02-03 20:40:18 +0000

    4 and 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts : print string
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  char My_msg[] = "\"Programming is like building a multilingual puzzle";
+
+  puts(My_msg);
+  return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf: prints a string
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  char My_msg[] = "with proper grammer, but the outcome is a piece of art,";
+
+  printf("%s\n", My_msg);
+  return (0);
+}
