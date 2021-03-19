9b5c67bc97a64024178969ec93605892ab4351bd
sateesh111 <hussensateesh@gmail.com>
2021-03-19 16:16:53 +0000

    advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * first - Function that prints before main
+ */
+void __attribute__((constructor)) first(void)
+{
+printf("%s",
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
